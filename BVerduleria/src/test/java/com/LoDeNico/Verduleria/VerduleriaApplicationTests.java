d243aa392e67d1017835360de50858dbeb0020e4
MrNarnia01 <brunonp03@gmail.com>
2024-05-21 14:52:32 -0300

    Base del Main para funcionar

---
+++ BVerduleria/src/test/java/com/LoDeNico/Verduleria/VerduleriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LoDeNico.Verduleria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerduleriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
