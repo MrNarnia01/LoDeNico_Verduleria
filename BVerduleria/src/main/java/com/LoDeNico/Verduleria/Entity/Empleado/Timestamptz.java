e5f44ff4e6cdc27e930651927547dda1b2e943a0
MrNarnia01 <brunonp03@gmail.com>
2024-06-21 19:11:02 -0300

    Service de Impl con Interface

--- BVerduleria/src/main/java/com/LoDeNico/Verduleria/Entity/Empleado/Timestamptz.java
+++
@@ -1,4 +0,0 @@
-package com.LoDeNico.Verduleria.Entity.Empleado;
-
-public class Timestamptz {
-}
--- BVerduleria/src/main/java/com/LoDeNico/Verduleria/Service/Empleado/HorarioService.java
+++ BVerduleria/src/main/java/com/LoDeNico/Verduleria/Service/Empleado/HorarioService.java
@@ -1,4 +1,14 @@
 package com.LoDeNico.Verduleria.Service.Empleado;
 
+import com.LoDeNico.Verduleria.Dto.Request.Empleado.HorarioRequest;
+import com.LoDeNico.Verduleria.Dto.Response.Empleado.HorarioResponse;
+
+import java.util.Date;
+
 public interface HorarioService {
+    public HorarioResponse getHorario(Long id);
+    public int deleteHorario(Long id);
+    public HorarioResponse createHorario(HorarioRequest horarioRequest);
+    public  HorarioResponse updateHorario(HorarioRequest horarioRequest, Long id);
+    public HorarioResponse cierreHorario(Long id, Date salida);
 }
